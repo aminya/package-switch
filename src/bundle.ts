db1b57ba5c2ed9fce89131d24c13ed8ec7e97520
aminya <aminyahyaabadi74@gmail.com>
2020-04-10 10:12:33 -0500

    Bundles removing addressed comments + make field public

--- src/bundle.ts
+++ src/bundle.ts
@@ -1,12 +1,5 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS206: Consider reworking classes to avoid initClass
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-
 export class Bundle {
-  private packages: any
+  packages: any
 
   constructor({ packages }) {
     this.packages = packages
