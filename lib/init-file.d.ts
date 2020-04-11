224153a657ddb0a4425a6f95c00d95a8b0c29213
aminya <aminyahyaabadi74@gmail.com>
2020-04-10 21:00:11 -0500

    Add built files

--- lib/init-file.d.ts
+++ lib/init-file.d.ts
@@ -1,5 +1,10 @@
 export declare class InitFile {
     constructor(name: any, filepath: any);
-    execute(opposite: any): any;
+    execute(opposite: any): void;
+    save(): void;
+}
+export declare class InitFileCSON {
+    constructor(name: any, filepath: any);
+    execute(opposite: any): void;
     save(): void;
 }
